5441bae49a7162e0f43e389782162d0c4e52c0ba
DESKTOP-RD2LRN2\Elahe <elahe.fahmideh@gmail.com>
2025-09-03 22:10:19 +0330

    fix bug

--- src/Components/NoteApp/NoteBody/NoteBody.jsx
+++ src/Components/NoteApp/NoteBody/NoteBody.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import './NoteBody.css'
-import NewNote from './NewNote/NewNote'
-import NoteList from './NoteList/NoteList'
+import "./NoteBody.css";
+import NewNote from "./NewNote/NewNote";
+import NoteList from "./NoteList/noteList";
 
+function NoteBody({ addNote, notes, deleteNote, handleCompleted, sortBy }) {
+  return (
+    <>
+      <div className="note-body">
+        <div className="new-note-section">
+          <NewNote onAddNote={addNote} />
+        </div>
 
-
-function NoteBody({ addNote, notes, deleteNote, handleCompleted ,sortBy}) {
-    return (
-        <>
-            <div className="note-body">
-
-                <div className="new-note-section">
-                    <NewNote onAddNote={addNote} />
-                </div>
-
-                <div className="note-list-section">
-                    <NoteList notes={notes} deleteNote={deleteNote} onComplete={handleCompleted} sortBy={sortBy}/>
-                </div>
-
-            </div >
-        </>
-    )
+        <div className="note-list-section">
+          <NoteList
+            notes={notes}
+            deleteNote={deleteNote}
+            onComplete={handleCompleted}
+            sortBy={sortBy}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default NoteBody
+export default NoteBody;
